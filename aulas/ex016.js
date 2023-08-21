e8ecfb08da3f05069b29b8fd85c2d9e42d104a48
Renan Fernandes <101608000+renanfersil@users.noreply.github.com>
2023-08-21 16:08:35 -0300

    Create ex016.js

---
+++ aulas/ex016.js
@@ -0,0 +1,8 @@
+let num = [4, 3, 2, 1]
+
+num.sort() //põe em ordem
+num.push(1) //adiciona um elemento 
+console.log(num)
+//console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posições.`) // identifica a quatidade de elementos
+console.log(`O primeiro valor do vetor é ${num[0]}`) // identifica o primeiro elemento, que é "0"
