fb66b785e2cec7fbbf0d466e2990e6fd92a6b1aa
Renan Fernandes <101608000+renanfersil@users.noreply.github.com>
2023-08-28 12:05:57 -0300

    update

--- aulas/ex016.js
+++ aulas/ex016.js
@@ -1,8 +1,14 @@
 let num = [4, 3, 2, 1]
 
+num.push(5) //adiciona um elemento 
 num.sort() //põe em ordem
-num.push(1) //adiciona um elemento 
 console.log(num)
 //console.log(`Nosso vetor é o ${num}`)
 console.log(`O vetor tem ${num.length} posições.`) // identifica a quatidade de elementos
 console.log(`O primeiro valor do vetor é ${num[0]}`) // identifica o primeiro elemento, que é "0"
+let pos = num.indexOf(8)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado!`)
+} else {
+    console.log(`O valor 4 está na posição ${pos}`)
+}
