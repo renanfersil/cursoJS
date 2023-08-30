dc715e6c454925681773cc05cae401f075acf529
Renan Fernandes <101608000+renanfersil@users.noreply.github.com>
2023-08-30 12:53:25 -0300

    update

--- modelo numerador/script.js
+++ modelo numerador/script.js
@@ -50,10 +50,12 @@ function finalizar() {
                 menor = valores[pos]
         }
 
+        media = soma / tot
         res.innerHTML = ''
         res.innerHTML += `<p>Ao todo, temos ${tot} número(s) cadastrado(s).</p>`
-        innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
-        innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
         res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
     } 
 }
